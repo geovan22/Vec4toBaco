85552e5e5fc95575655bf286338c15c5e5e3b17d
geovan22 <geovan221315@hotmail.com>
2021-06-07 12:19:29 -0600

    Carga datos  vectos

--- APP_VEC/APP_VEC/Program.cs
+++ APP_VEC/APP_VEC/Program.cs
@@ -9,8 +9,16 @@ namespace APP_VEC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola");
-            Console.WriteLine("Como estan");
+            char[] dato = new char[10];
+
+            //dato[0] = 'a'; 
+
+            for (int i=0; i<=9; i++)
+            {
+                Console.WriteLine("i: "+i);
+                Console.Write("Ingrese una letra: ");
+                dato[i] = char.Parse(Console.ReadLine());
+            }
 
             Console.ReadKey();
         }
